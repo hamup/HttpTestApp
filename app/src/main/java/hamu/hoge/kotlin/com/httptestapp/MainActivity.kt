8035f4c657f19ddca8bb23dc88f77479eac69d67
hamu <gc.kai.g@gmail.com>
2018-07-24 00:06:30 +0900

    Add Http Communication Logic

--- app/src/main/java/hamu/hoge/kotlin/com/httptestapp/MainActivity.kt
+++ app/src/main/java/hamu/hoge/kotlin/com/httptestapp/MainActivity.kt
@@ -2,11 +2,46 @@ package hamu.hoge.kotlin.com.httptestapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    val httpClient = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textview = findViewById<TextView>(R.id.textView)
+        val getbutton = findViewById<Button>(R.id.get_button)
+        val handler = Handler()
+
+        getbutton.setOnClickListener {
+            run("https://everyday-growth.com/name/api/?cnt=2", handler, textview)
+        }
+    }
+
+    fun run(url: String, handler: Handler, textView: TextView) {
+        val request = Request.Builder()
+                .addHeader("Content-Type", "text/plain; charset=utf-8")
+                .url(url)
+                .get()
+                .build()
+
+        httpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                //TODO : Add Error Handling
+            }
+            override fun onResponse(call: Call, response: Response) {
+                val responseText: String? = response.body()?.string()
+                handler.post {
+                    textView.text = responseText
+                }
+            }
+        })
     }
 }
